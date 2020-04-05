93d0f44f224bc5b4b641e2bc270f848b9754181c
Xiaolin <xiaolin@MacEnvironment.local>
2020-04-05 17:56:24 +0200

    Can't fix build the tink link, use hard code

--- server.py
+++ server.py
@@ -1,14 +1,13 @@
 import configparser
-import utils
 from flask import Flask, request, render_template, redirect
 from werkzeug.urls import iri_to_uri
+from requests.models import PreparedRequest
 
 app = Flask(__name__)
 app.debug = True
 config = configparser.ConfigParser()
 config.read('properties.ini')
-#tinkURL= utils.generateTinkLink()
-tinkURL= 'https://link.tink.com/1.0/authorize/?client_id=51f92b60022c4b70a29bee5da58aae62&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&scope=accounts:read,transactions:read,user:read&market=SE&locale=en_US'
+tinkURL = 'https://link.tink.com/1.0/authorize/?client_id=51f92b60022c4b70a29bee5da58aae62&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&scope=accounts:read,transactions:read,user:read&market=SE&locale=en_US'
 
 @app.route('/')
 def home():
@@ -16,6 +15,19 @@ def home():
 
 @app.route('/tinklink')
 def tinkLink():
+    # req = PreparedRequest()
+    # redirect_uri = 'http://' + config['Local']['CallBackAddr'] + ':' + config['Local']['CallBackPort'] + config['Local']['CallBackDir']
+    # scope = "accounts:read,transactions:read,user:read"
+    # params = {
+    #     'client_id': config['Tink']['ClientId'], 
+    #     'redirect_uri': redirect_uri,
+    #     'scope': scope, 
+    #     'market': config['Tink']['Market'], 
+    #     'locale': config['Tink']['Locale']
+    #     }
+    
+    # req.prepare_url(config['Tink']['BaseUrl'], params)
+    # return redirect(req.url)
     return redirect(tinkURL)
 
 @app.route('/' + config['Local']['CallBackDir'])
